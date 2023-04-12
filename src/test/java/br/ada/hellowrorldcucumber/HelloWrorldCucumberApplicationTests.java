0e24c9577b28248005e6112daf8aa81511b38aa7
William Cesar Santos <william_cesar_santos@hotmail.com>
2023-04-12 19:56:33 -0300

    Update

---
+++ src/test/java/br/ada/hellowrorldcucumber/HelloWrorldCucumberApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ada.hellowrorldcucumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWrorldCucumberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
