383633772b2d2f80bb05bb3baa0ab1b08e192d40
William Cesar Santos <william_cesar_santos@hotmail.com>
2023-04-12 21:08:13 -0300

    Update

---
+++ src/test/java/br/ada/helloworldcucumber/AlreadyHadDinner.java
@@ -0,0 +1,33 @@
+package br.ada.helloworldcucumber;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
+
+public class AlreadyHadDinner {
+
+    private String dessert;
+
+    @Given("After eat my dinner")
+    public void afterDinner() {
+
+    }
+
+    @When("I find my dessert")
+    public void findMyDessert() {
+    }
+
+    @And("I found {string}")
+    public void foundDessert(String dessert) {
+        this.dessert = dessert;
+        System.out.println("I will eat " + dessert);
+    }
+
+    @Then("I aet {string}")
+    public void iAetIceCream(String dessert) {
+        Assertions.assertEquals(dessert, this.dessert);
+    }
+
+}
---
+++ src/test/resources/features/i_ve_already_had_dinner.feature
@@ -0,0 +1,6 @@
+Feature: I'm already had aet
+  Scenario: After aet
+    Given After eat my dinner
+    When I find my dessert
+    And I found "Ice cream"
+    Then I aet "Ice cream"
