96f746805492878616d2c07f95db4ea315b0e3c6
William Cesar Santos <william_cesar_santos@hotmail.com>
2023-04-12 21:09:40 -0300

    Update

--- src/test/java/br/ada/helloworldcucumber/AlreadyHadDinnerDefinitions.java
+++ src/test/java/br/ada/helloworldcucumber/AlreadyHadDinnerDefinitions.java
@@ -6,7 +6,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
 
-public class AlreadyHadDinner {
+public class AlreadyHadDinnerDefinitions {
 
     private String dessert;
 
