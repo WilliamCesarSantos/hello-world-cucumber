f0dd54378015f93629f0f8f3fedfacc7d2b60c23
William Cesar Santos <william_cesar_santos@hotmail.com>
2023-04-12 21:30:55 -0300

    Update

--- src/test/java/br/ada/helloworldcucumber/AlreadyHadDinnerDefinitions.java
+++ src/test/java/br/ada/helloworldcucumber/AlreadyHadDinnerDefinitions.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Assertions;
 public class AlreadyHadDinnerDefinitions {
 
     private String dessert;
+    private boolean amISad = false;
 
     @Given("After eat my dinner")
     public void afterDinner() {
@@ -25,9 +26,31 @@ public class AlreadyHadDinnerDefinitions {
         System.out.println("I will eat " + dessert);
     }
 
+    @And("I open pot and the content was {string}")
+    public void content(String content) {
+        System.out.println("I found " + content);
+        if (content.equalsIgnoreCase("ice cream")) {
+            amISad = false;
+        } else {
+            amISad = true;
+        }
+    }
+
     @Then("I aet {string}")
     public void iAetIceCream(String dessert) {
         Assertions.assertEquals(dessert, this.dessert);
+    }
+
+    @Then("I'm very sad now")
+    public void iamSad() {
+        Assertions.assertTrue(amISad);
+        System.out.println("I'm very sad");
+    }
+
+    @Then("I'm very happy now")
+    public void iamHappy() {
+        Assertions.assertFalse(amISad);
+        System.out.println("I'm very happy");
     }
 
 }
